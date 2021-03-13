3afcbe988e2a9ae4a3c7650bdf85336a32f01c7f
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2021-03-13 12:17:53 +0800

    Added RF and sound level monitoring
    + RF initialization
    + Hydrophone initialization
    + Serial baud rate
    + Sound level measurement

--- src/buoy/buoy.ino
+++ src/buoy/buoy.ino
@@ -11,15 +11,42 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-const int mic = A0; // Pin for hydrophone
+// Tansceiver variables
+#define CE_PIN   9
+#define CSN_PIN 10
+const byte receiveAddress[5] = {'R','x','A','A','A'};
+//RF24 radio(CE_PIN, CSN_PIN);
+
+// Hydrophone variables
+const int mic = A0;
+int noiseThreshold;
 
 void setup() {
+	Serial.begin(115200);
+	Serial.println("Buoy Initialized");
+	pinMode(13, OUTPUT);
 }
 
 void loop() {
-    long soundLevel = 0;
-    for(int i=0; i<32; i++) {
-        soundLevel += analogRead(pinAdc);
-    }
-    soundLevel >>= 5;
+	long soundLevel = 0;
+	readSoundLevel(&soundLevel);
+	if (soundLevel > noiseThreshold)
+		warnStation();
+	delay(10);
+}
+
+/**
+* Reads the analog sound level being picked up by the mic and saves its value.
+*/
+void readSoundLevel(long * level) {
+	for(int i=0; i<32; i++)
+		*level += analogRead(mic);
+	*level >>= 5;
+}
+
+/**
+* Transmit a warning signal to the station.
+*/
+void warnStation() {
+
 }
