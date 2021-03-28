dde4c26bc2d1e68c69d0dbd128a48619b2d6872d
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2021-03-28 14:01:33 +0800

    Created packet class

---
+++ src/buoy-ver2/Packet.cpp
@@ -0,0 +1,60 @@
+#include "Packet.h"
+#include "Arduino.h"
+
+byte * Packet::getRawData() {
+	return data;
+}
+
+void Packet::setRawData(byte in[]) {
+	for (byte i = 0; i < 3; i++)
+		data[i] = in[i];
+}
+
+/**
+ * Gets the byte portion of the packet.
+ *
+ * @return the byte portion of the packet.
+ */
+byte Packet::getByte() {
+	return data[0];
+}
+
+/**
+* Puts a byte into the outbox.
+*
+* @param data is the byte to be put into the command address in the outbox.
+*/
+void Packet::storeByte(byte value) {
+	data[0] = value;
+}
+
+/**
+* Gets the unsigned integer from the packet.
+* This combines the last 2 bytes in the packet into an unsigned int.
+*
+* @return is the unsigned integer resulting from the combination of the
+* last 2 bytes of incomingPacket.
+*/
+unsigned int Packet::getUnsignedInt() {
+	return (unsigned int) (data[2]<<8) | (data[1]);
+}
+
+/**
+* Splits an unsigned integer into two bytes and stores them in the packet.
+*
+* @param num is the unsigned integer to be stored in the packet.
+*/
+void Packet::storeUnsignedInt(unsigned int num) {
+	data[1] = (byte) (num & 0xFF);
+	data[2] = (byte) ((num >> 8) & 0xFF);
+}
+
+/**
+ * Resets all the bytes in the packet.
+ */
+void Packet::reset() {
+	for (byte i = 0; i < 3; i++)
+		data[i] = 0;
+}
+
+
---
+++ src/buoy-ver2/Packet.h
@@ -0,0 +1,28 @@
+#ifndef H_PACKET
+#define H_PACKET
+
+#include "Arduino.h"
+
+class Packet {
+	private:
+		/**
+		 * Packets are 3 bytes in size.
+		 * The first byte is for the "command byte" which corresponds to
+		 * a specific command. The last 2 bytes are for unsigned integer
+		 * values that are relevant to the command byte, particularly for
+		 * sending data back to the station.
+		 */
+		byte data[3];
+
+	public:
+		byte * getRawData();
+		void setRawData(byte in[]);
+		byte getByte();
+		void storeByte(byte value);
+		unsigned int getUnsignedInt();
+		void storeUnsignedInt(unsigned int num);
+		void reset();
+};
+
+#endif
+
