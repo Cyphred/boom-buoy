5f4522f2994e355ce7f83b8a7de581313b6c9e11
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2021-04-06 17:45:43 +0800

    Copied common classes between buoy and station

---
+++ src/station/Buzzer.h
@@ -0,0 +1,16 @@
+#ifndef H_BUZZER
+#define H_BUZZER
+
+#include "Arduino.h"
+
+class Buzzer {
+	private:
+		int pin;
+	public:
+		Buzzer(int pin);
+		void initSuccess();
+		void disconnected();
+		void radioError();
+};
+
+#endif
