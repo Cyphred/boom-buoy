94876c414c15506759fafa05c62f1c142927bd78
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2021-04-07 15:21:53 +0800

    Moved station constants and created constats for console

---
+++ src/station/Console.ino
@@ -0,0 +1,43 @@
+/**
+* Methods and variables for interfacing with the console application.
+*/
+void interpretConsoleCommand(byte command) {
+	switch (command) {
+		case (byte) CON_CONNECT:
+			consoleConnect();
+			break;
+		case (byte) CON_DISCONNECT:
+			break;
+		case (byte) CON_SET_VARIANCETHRESHOLD:
+			break;
+		case (byte) CON_SET_MEASUREMENTINTERVAL:
+			break;
+		case (byte) CON_SET_PINGTIMEOUT:
+			break;
+		case (byte) CON_SET_MODE:
+			break;
+		case (byte) CON_GET_VARIANCETHRESHOLD:
+			break;
+		case (byte) CON_GET_MEASUREMENTINTERVAL:
+			break;
+		case (byte) CON_GET_PINGTIMEOUT:
+			break;
+		case (byte) CON_GET_MODE:
+			break;
+	}
+}
+
+/**
+* Handles request for connection by a console application.
+*/
+void consoleConnect() {
+	if (connectedToConsole) {
+		buzzer.genericError();
+		Serial.write(CON_REJECT);
+	}
+	else {
+		connectedToConsole = true;
+		Serial.write(CON_ACCEPT);
+		buzzer.genericOK();
+	}
+}
