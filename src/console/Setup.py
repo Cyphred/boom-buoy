89ea6563910d4e9c01f2f99f973fbfe06171e38a
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2021-04-07 12:57:00 +0800

    Fixed connection establishment

--- src/console/Setup.py
+++ src/console/Setup.py
@@ -1,28 +1,57 @@
 import os.path
 import serial.tools.list_ports
+import Device
+import time
 
-# Checks if the device is valid
-def isValidDevice(device):
-    if not _deviceExists(device):
-        print(f"ERROR: \"{device}\" does not exist.")
-        return False
-
-    if not _deviceIsCOM(device):
-        print(f"ERROR: \"{device}\" is not a valid COM device.")
-        return False
-
-    return True
-
-# Checks if the device exists
-def _deviceExists(device):
-    if os.path.exists(device):
+# Checks if the device path exists
+def devicePathExists(devicePath):
+    print("Checking if device path is valid...", end="")
+    if os.path.exists(devicePath):
+        print("OK")
         return True
     else:
+        print("FAILED")
+        print(f"\nERROR: Device \"{devicePath}\" does not exist.")
         return False
 
-# Checks if the device is a valid COM device
-def _deviceIsCOM(device):
+# Checks if the device in the path is a valid COM device
+def devicePathIsCOM(devicePath):
+    print("Verifying COM device...", end="")
     ports = [tuple(p) for p in list(serial.tools.list_ports.comports())]
-    if not device in ports[0] and not device in ports[1]:
+    if devicePath in ports[0] or devicePath in ports[1]:
+        print("OK")
+        return True
+    else:
+        print("FAILED")
+        print(f"\nERROR: \"{devicePath}\" is not a valid COM device.")
         return False
-    return True
+
+# Sends a magic byte to the device and checks if it sends
+# the appropriate response.
+def authenticateWithDevice(device, timeout):
+    print("Authenticating...", end="")
+
+    reply = None
+    timeout /= 1000
+    start = time.time()
+
+    # Send connection requests to device until a response is received,
+    # or until the allowed time expires.
+    while not reply and (time.time() - start) < timeout:
+        device.write('g')
+        reply = device.read()
+
+    # reply is empty if no response has been received
+    if not reply:
+        print("FAILED")
+        print("ERROR: Device took to long to respond.")
+        return False
+
+    # if the magic byte is received
+    if reply == 'g':
+        print("OK")
+        return True
+    else:
+        print("FAILED")
+        print("ERROR: Device sent an invalid response. Are you sure you selected the correct device?")
+        return False
