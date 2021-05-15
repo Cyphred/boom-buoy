bb1b9c18e717cca3dae9217592a4e918bc30c2a0
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2021-05-15 14:14:25 +0800

    Renamed radio variable

--- src/common/Radio.cpp
+++ src/common/Radio.cpp
@@ -8,24 +8,26 @@
  * @param rxAddress is the array of bytes containing the receive address for incoming data.
  * @param txAddress is the array of bytes containing the transmit address for outgoing data.
  */
-Radio::Radio(byte ce, byte csn, byte * rx, byte * tx) : rf(ce, csn) {
+Radio::Radio(int ce, int csn, byte * rx, byte * tx) : m_radio(ce, csn) {
 	// Addresses should always be 5 bytes in length.
 	for (byte i = 0; i < 5 ; i++ ) {
 		rxAddress[i] = rx[i];
 		txAddress[i] = tx[i];
 	}
 
-	rf.begin();
-	if ( rf.isChipConnected() ) {
-		rf.setDataRate( RF24_250KBPS );
-		rf.openWritingPipe( txAddress );
-		rf.openReadingPipe( 1, rxAddress );
-		rf.setRetries( DELAY, MAX_RETRIES );
-		rf.startListening();
+	m_radio.begin();
+	if ( m_radio.isChipConnected() ) {
+		m_radio.setDataRate( RF24_250KBPS );
+		m_radio.openWritingPipe( txAddress );
+		m_radio.openReadingPipe( 1, rxAddress );
+		m_radio.setRetries( DELAY, MAX_RETRIES );
+		m_radio.startListening();
 		initialized = true;
+		Serial.println("Radio initialized");
 	}
 	else {
 		initialized = false;
+		Serial.println("Radio NOT initialized");
 	}
 }
 
@@ -35,7 +37,7 @@ Radio::Radio(byte ce, byte csn, byte * rx, byte * tx) : rf(ce, csn) {
 * @param *packet is the pointer to the packet where the received data will be written to.
 */
 void Radio::storeReceivedDataIn(Packet * packet) {
-	rf.read( &packet->data, sizeof( packet->data ) ); // Read data from rf buffer and store it into packet's array.
+	m_radio.read( &packet->data, sizeof( packet->data ) ); // Read data from rf buffer and store it into packet's array.
 }
 
 /**
@@ -45,9 +47,9 @@ void Radio::storeReceivedDataIn(Packet * packet) {
  * @return true if an acknowledgement has been received.
  */
 bool Radio::transmit(Packet * packet) {
-	rf.stopListening();
-	bool acknowledged = rf.write( &packet->data, sizeof( packet->data ) );
-	rf.startListening();
+	m_radio.stopListening();
+	bool acknowledged = m_radio.write( &packet->data, sizeof( packet->data ) );
+	m_radio.startListening();
 	if ( acknowledged );
 		return true;
 	return false;
@@ -68,5 +70,5 @@ bool Radio::isInitialized() {
  * @return is true if there is data in the receive buffer.
  */
 bool Radio::isDataAvailable() {
-	return rf.available();
+	return m_radio.available();
 }
